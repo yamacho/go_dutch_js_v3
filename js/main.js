3e8c8bf3e29dcf4f9120a091be623d8e1b7122ec
yamacho <yamacho@users.noreply.github.com>
2017-11-17 18:02:31 +0900

    要素の取得をしていこう
    * main.js の読み込み
    * 要素の取得

---
+++ js/main.js
@@ -0,0 +1,13 @@
+(function() {
+  'use strict';
+
+  var price = document.getElementById('price');
+  var num = document.getElementById('num');
+  var unit = document.getElementById('unit');
+  var btn = document.getElementById('btn');
+  var result = document.getElementById('result');
+  var reset = document.getElementById('reset');
+
+  btn.addEventListener('click', function() {
+  });
+})();
