422ddff7ebf55620e79fa5074850220b16de27ff
yamacho <yamacho@users.noreply.github.com>
2017-11-17 18:15:34 +0900

    計算式を完成させよう
    * 割り勘機能の実装

--- js/main.js
+++ js/main.js
@@ -9,5 +9,19 @@
   var reset = document.getElementById('reset');
 
   btn.addEventListener('click', function() {
+    var payLess;
+    var short;
+    var payMore;
+    var over;
+    var str;
+
+    payLess = Math.floor((price.value / num.value) / unit.value) * unit.value;
+    short = price.value - (payLess * num.value);
+    payMore = Math.ceil((price.value / num.value) / unit.value) * unit.value;
+    over = Math.abs(price.value - (payMore * num.value));
+    str =
+      '一人 ' + payLess + ' 円だと ' + short + ' 円足りません。' +
+      '一人 ' + payMore + ' 円だと ' + over + ' 円余ります。';
+    result.textContent = str;
   });
 })();
